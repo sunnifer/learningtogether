16f7e4c6d7585ea06b4a66d25194c50dd51a5b94
sunnifer <jsun1292@gmail.com>
2015-06-16 00:53:12 -0400

    oops - this is actually the saved work for 3a. to be continued…

---
+++ assignments/conditionals-loops-arrays/3a-drunk-walk.php
@@ -0,0 +1,52 @@
+<?php
+
+// A drunkard begins walking aimlessly, starting at a lamp post. At each time step, the drunkard forgets where he or she is, and takes one step at random, either north, east, south, or west, with probability 25%. How far will the drunkard be from the lamp post after N steps?
+
+// Write a program that
+// (a) takes an integer command-line argument N and simulates the motion of a random walker for N steps.
+// (b) After each step, print the location of the random walker, treating the lamp post as the origin (0, 0).
+// (c) Also, print the square of the final distance from the origin.
+
+// (a) create integer command-line arg N that dictates the number of steps taken; also create var $direction, $x, $y.
+
+$steps = $argv[1];
+
+// set and print the current location
+
+function print_location($x,$y) {
+  $direction = mt_rand(1,4);
+  // echo $direction  . "\n";
+  switch ($direction) {
+    case 1: // move north
+      $y = ++$y;
+      break;
+    case 2: // move south
+      $y = --$y;
+      break;
+    case 3: // move east
+      $x = ++$x;
+      break;
+    case 4: // move west
+      $x = --$x;
+      break;
+    default:
+      echo "There's been an inexplicable error.";
+  }
+    echo "(" . $x . ", " . $y . ")" . "\n";
+}
+
+// (b) create while loop, where while $steps>0, repeat print_location
+
+while ($steps>0) {
+  print_location($x,$y);
+  $steps = --$steps;
+}
+
+// when $steps=0, print the square of the final distance from origin
+
+$xSquare = $x*$x;
+echo $xSquare;
+
+echo "squared distance = ";
+
+?>
